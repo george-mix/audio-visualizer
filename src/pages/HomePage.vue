ff9961876b252b8b7690ec8bb9b9f9050736ab56
Georgy <gosha.gosha9696@gmail.com>
2024-06-28 23:31:45 +0300

    build: add css processors

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,5 +1,3 @@
 <template>
   <div>Home page</div>
 </template>
-
-<script setup lang="ts"></script>
--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,18 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import autoprefixer from 'autoprefixer'
+import postCssPresetEnv from 'postcss-preset-env'
 
 // https://vitejs.dev/config/
 /** @type {import('vite').UserConfig} */
 export default defineConfig({
-  plugins: [
-    vue(),
-  ],
+  plugins: [vue()],
+  css: {
+    postcss: {
+      plugins: [autoprefixer({}), postCssPresetEnv({})]
+    }
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
